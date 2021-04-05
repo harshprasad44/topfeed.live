621bdf74ece92fe88ae5c87f16002edae3ada3bb
harshprasad44 <explorerharsh@gmail.com>
2021-04-05 22:46:15 +0530

    adding tool bar and feed page

--- pages/eom.js
+++ pages/eom.js
@@ -1,8 +1,10 @@
+import { Toolbar } from "../components/toolbar";
 import styles from "../styles/EOM.module.css";
 
 const eom = ({ employee }) => {
   return (
     <div className="page-container">
+      <Toolbar />
       <div className={styles.main}>
         <h1>Employee of the month!</h1>
 
---
+++ pages/feed/[slug].js
@@ -0,0 +1,46 @@
+import styles from "../../styles/Feed.module.css";
+
+const Feed = ({ pageNumber, articles }) => {
+  return (
+    <div className={styles.main}>
+      {articles.map((article, index) => (
+        <div key={index} className={styles.post}>
+          <h1 onClick={() => window.open(`${article.url}`, "_blank")}>{article.title}</h1>
+          <p>{article.description}</p>
+          {!!article.urlToImage && <img style={{ cursor: "pointer" }} onClick={() => window.open(`${article.url}`, "_blank")} src={article.urlToImage} />}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const pageNumber = pageContext.query.slug;
+
+  if (!pageNumber || pageNumber < 1 || pageNumber > 5) {
+    return {
+      props: {
+        articles: [],
+        pageNumber: 1,
+      },
+    };
+  }
+
+  const apiResponse = await fetch(`https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+    },
+  });
+
+  const apiJson = await apiResponse.json();
+  const { articles } = apiJson;
+
+  return {
+    props: {
+      articles,
+      pageNumber: Number.parseInt(pageNumber),
+    },
+  };
+};
+
+export default Feed;
