4ca15e664ea05e339d98c535ca245567b3c39576
harshprasad44 <explorerharsh@gmail.com>
2021-04-06 14:47:43 +0530

    Pagination

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,15 +1,48 @@
+import { useRouter } from "next/router";
+import { Toolbar } from "../../components/toolbar";
 import styles from "../../styles/Feed.module.css";
 
 const Feed = ({ pageNumber, articles }) => {
+  const router = useRouter();
+
   return (
-    <div className={styles.main}>
-      {articles.map((article, index) => (
-        <div key={index} className={styles.post}>
-          <h1 onClick={() => window.open(`${article.url}`, "_blank")}>{article.title}</h1>
-          <p>{article.description}</p>
-          {!!article.urlToImage && <img style={{ cursor: "pointer" }} onClick={() => window.open(`${article.url}`, "_blank")} src={article.urlToImage} />}
+    <div className="page-container">
+      <Toolbar />
+      <div className={styles.main}>
+        {articles.map((article, index) => (
+          <div key={index} className={styles.post}>
+            <h1 onClick={() => window.open(`${article.url}`, "_blank")}>{article.title}</h1>
+            <p>{article.description}</p>
+            {!!article.urlToImage && <img style={{ cursor: "pointer" }} onClick={() => window.open(`${article.url}`, "_blank")} src={article.urlToImage} />}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.paginator}>
+        <div
+          onClick={() => {
+            if (pageNumber > 1) {
+              router.push(`/feed/${pageNumber - 1}`);
+            }
+          }}
+          className={pageNumber === 1 ? styles.disabled : styles.active}
+        >
+          Previous Page
         </div>
-      ))}
+
+        <div>#{pageNumber}</div>
+
+        <div
+          onClick={() => {
+            if (pageNumber < 5) {
+              router.push(`/feed/${pageNumber + 1}`);
+            }
+          }}
+          className={pageNumber === 5 ? styles.disabled : styles.active}
+        >
+          Next Page
+        </div>
+      </div>
     </div>
   );
 };
