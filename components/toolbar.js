db54b851a2379e1a665e7945a599fd430502baa1
harshprasad44 <explorerharsh@gmail.com>
2021-04-10 21:08:53 +0530

    adding background & toolbar underlines

--- components/toolbar.js
+++ components/toolbar.js
@@ -3,13 +3,37 @@ import styles from "../styles/Toolbar.module.css";
 
 export const Toolbar = () => {
   const router = useRouter();
+  let feed = router.pathname.split("/").includes("feed");
+  let home = router.pathname === "/";
 
   return (
     <div className={styles.main}>
-      <div onClick={() => router.push("/")}>Home</div>
-      <div onClick={() => router.push("/feed/1")}>Feed</div>
+      {home ? (
+        <div
+          style={{ marginBottom: "1px", borderBottom: "black solid" }}
+          onClick={() => router.push("/")}
+        >
+          Home
+        </div>
+      ) : (
+        <div onClick={() => router.push("/")}>Home</div>
+      )}
+
+      {feed ? (
+        <div
+          style={{ marginBottom: "1px", borderBottom: "black solid" }}
+          onClick={() => router.push("/feed/1")}
+        >
+          Feed
+        </div>
+      ) : (
+        <div onClick={() => router.push("/feed/1")}>Feed</div>
+      )}
+
       <div onClick={() => router.push("/eom")}>EOM</div>
-      <div onClick={() => (window.location.href = "https://www.google.com")}>Twitter</div>
+      <div onClick={() => (window.location.href = "https://www.google.com")}>
+        Twitter
+      </div>
     </div>
   );
 };
